1bdf5b352a4ebc07bddde3343daac79714c40be1
abhinext2244 <abhisheknextabhi@gmail.com>
2026-04-13 17:56:44 +0530

    feat: add JWT handler with generateToken and verifyToken functions

---
+++ src/utils/jwt.ts
@@ -0,0 +1,28 @@
+import crypto from "node:crypto";
+import jwt from "jsonwebtoken";
+ const JWT_SECRET_ACEESS_TOKEN = process.env.JWT_SECRET_ACEESS_TOKEN as string;
+ const JWT_SECRET_REFRESH_TOKEN = process.env.JWT_SECRET_REFRESH_TOKEN as string;
+ export interface JwtPayload {
+     id: string;
+     email: string;
+   
+ }
+ const  generateAccessToken = (Payload:JwtPayload) => {
+     return jwt.sign({ Payload }, JWT_SECRET_ACEESS_TOKEN, { expiresIn: "15m" });
+ };
+ const verifyAccessToken = (token: string): JwtPayload => {
+     return jwt.verify(token, JWT_SECRET_ACEESS_TOKEN)as JwtPayload;
+ }
+ const  generateRefreshToken = (id: string, email: string) => {
+     return jwt.sign({ id, email }, JWT_SECRET_REFRESH_TOKEN, { expiresIn: "7d" });
+ };
+ 
+ const verifyRefreshToken = (token: string): JwtPayload => {
+     return jwt.verify(token, JWT_SECRET_REFRESH_TOKEN)as JwtPayload;
+ }
+ const generateResetToken = () => {
+     const rawToken= crypto.randomBytes(32).toString("hex");
+     const hashedToken = crypto.createHash("sha256").update(rawToken).digest("hex");
+     return {rawToken, hashedToken};
+ }
+ export { generateAccessToken, verifyAccessToken, generateRefreshToken, verifyRefreshToken };
