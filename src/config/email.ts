e3e34a9bd78b228dda6a3fd36eef26b358560938
abhinext2244 <abhisheknextabhi@gmail.com>
2026-04-15 17:08:49 +0530

    fix:resolve port configuration  issue

--- src/config/email.ts
+++ src/config/email.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer'
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: 587,
+  port: 2525,
   secure: false, 
   auth: {
     user: process.env.SMTP_USER,
