f7cd52698517c4d49c54396c34788e6702ea15ee
abhinext2244 <abhisheknextabhi@gmail.com>
2026-04-16 18:27:19 +0530

    add login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,9 +3,12 @@ import express, {Router} from "express";
 import * as authController from "../controllers/auth.controller";
 import { validate } from "../middleware/validate.middleware";
 import { registerSchema } from "../dto/auth.dto";
+import { loginSchema } from "../dto/auth.dto";
 
 const router: Router = express.Router();
 
 router.post("/register",validate(registerSchema), authController.register);
+router.post("/login",validate(loginSchema), authController.login);
+
 
 export default router;
