b39e84f403586a51e0d64379c769376ee1644ed9
abhinext2244 <abhisheknextabhi@gmail.com>
2026-04-16 17:32:11 +0530

    fix(auth): correct jwt payload and typing structure

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,8 +4,12 @@ import jwt from "jsonwebtoken";
  const JWT_SECRET_REFRESH_TOKEN = process.env.JWT_SECRET_REFRESH_TOKEN as string;
  export interface JwtPayload {
      id: string;
-     email: string;
+     role: string;
    
+ }
+ export interface RefreshTokenPayload {
+     id: string;
+     
  }
  const  generateAccessToken = (Payload:JwtPayload) => {
      return jwt.sign({ Payload }, JWT_SECRET_ACEESS_TOKEN, { expiresIn: "15m" });
@@ -13,12 +17,12 @@ import jwt from "jsonwebtoken";
  const verifyAccessToken = (token: string): JwtPayload => {
      return jwt.verify(token, JWT_SECRET_ACEESS_TOKEN)as JwtPayload;
  }
- const  generateRefreshToken = (id: string, email: string) => {
-     return jwt.sign({ id, email }, JWT_SECRET_REFRESH_TOKEN, { expiresIn: "7d" });
+ const  generateRefreshToken = (Payload:RefreshTokenPayload) => {
+     return jwt.sign({ Payload }, JWT_SECRET_REFRESH_TOKEN, { expiresIn: "7d" });
  };
  
- const verifyRefreshToken = (token: string): JwtPayload => {
-     return jwt.verify(token, JWT_SECRET_REFRESH_TOKEN)as JwtPayload;
+ const verifyRefreshToken = (token: string): RefreshTokenPayload => {
+     return jwt.verify(token, JWT_SECRET_REFRESH_TOKEN)as RefreshTokenPayload;
  }
  const generateResetToken = () => {
      const rawToken= crypto.randomBytes(32).toString("hex");
