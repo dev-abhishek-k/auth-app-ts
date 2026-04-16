15163845f844653043b5c1590c215793adadb190
abhinext2244 <abhisheknextabhi@gmail.com>
2026-04-16 18:19:40 +0530

    fix(auth): correct jwt secret config and payload structure

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,6 @@
 import crypto from "node:crypto";
 import jwt from "jsonwebtoken";
- const JWT_SECRET_ACEESS_TOKEN = process.env.JWT_SECRET_ACEESS_TOKEN as string;
+ const JWT_SECRET_ACCESS_TOKEN = process.env.JWT_SECRET_ACCESS_TOKEN as string;
  const JWT_SECRET_REFRESH_TOKEN = process.env.JWT_SECRET_REFRESH_TOKEN as string;
  export interface JwtPayload {
      id: string;
@@ -12,10 +12,10 @@ import jwt from "jsonwebtoken";
      
  }
  const  generateAccessToken = (Payload:JwtPayload) => {
-     return jwt.sign({ Payload }, JWT_SECRET_ACEESS_TOKEN, { expiresIn: "15m" });
+     return jwt.sign({ Payload }, JWT_SECRET_ACCESS_TOKEN, { expiresIn: "15m" });
  };
  const verifyAccessToken = (token: string): JwtPayload => {
-     return jwt.verify(token, JWT_SECRET_ACEESS_TOKEN)as JwtPayload;
+     return jwt.verify(token, JWT_SECRET_ACCESS_TOKEN)as JwtPayload;
  }
  const  generateRefreshToken = (Payload:RefreshTokenPayload) => {
      return jwt.sign({ Payload }, JWT_SECRET_REFRESH_TOKEN, { expiresIn: "7d" });
