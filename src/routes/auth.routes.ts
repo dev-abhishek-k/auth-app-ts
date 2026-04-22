9ef956180a5c0da5b138f2bd0d06c1125a3e886f
abhinext2244 <abhisheknextabhi@gmail.com>
2026-04-22 16:16:59 +0530

    add logout,verify-email,forgot-password,reset-password router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -10,6 +10,9 @@ const router: Router = express.Router();
 router.post("/register",validate(registerSchema), authController.register);
 router.post("/login",validate(loginSchema), authController.login);
 router.post("/refresh-token", authController.refreshToken);
-
+router.post("/logout", authController.logout);
+router.get("/verify-email", authController.verifyEmail);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password", authController.resetPassword);
 
 export default router;
